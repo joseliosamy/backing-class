e9d5e4a32e45b2e1447b6f62fabc017022d39868
Josélio Samy <joseliobackup@gmail.com>
2020-11-25 07:34:34 -0300

    Started remake in  user-page

--- scripts/login.js
+++ scripts/login.js
@@ -1,24 +1,33 @@
 
 function entrar(){
     const nome = document.getElementById("inputName").value; // observa o valor de #inputName   
+    var msg2 = document.querySelector(".msg2")
+    var msg = document.querySelector(".msg");
+    var msg3 = document.querySelector(".msg3")
     if(nome.length == 0){ // condiciona o nome a vazio, caso seja true, executa o codigo abaixo 
         var msg = document.querySelector(".msg"); //observa o valor de .labelSubmito 
         msg.setAttribute("id", "msg")
+        msg2.removeAttribute("id")
+        msg3.removeAttribute("id")
 
-        
     }
     // TRATAMENTO DE TAMANHO DO MÍNIMO DO NOME
     else if(nome.length > 0 && nome.length <= 3){
         // REMOÇÃO DO ALERTA DE USER NULOa
-        var msg3 = document.querySelector(".msg3")
-        msg3.setAttribute("id", "msg")          
+
+        msg3.setAttribute("id", "msg")   
+        msg2.removeAttribute("id")
+        msg.removeAttribute("id")
     } 
 
     // TRATAMENTO DE TAMANHO MÁXIMO DO NOME
     else if(nome.length >= 12){
- 
-        var msg2 = document.querySelector(".msg2")
+
         msg2.setAttribute("id", "msg")
+        msg.removeAttribute("id")
+        msg3.removeAttribute("id")
+
+       
     }
 
     // TRATAMENTO DE NÚMEROS NOS NOMES 
